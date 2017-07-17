987622c04685d74e6f902a30e3e788d9a68aa690
Ian Escarro <ian.escarro@gmail.com>
2017-07-17 21:33:23 +0800

    Updated love calculation

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,65 +9,109 @@ export class AppComponent {
   n1 = "";
   n2 = "";
 
+  n1love = "";
+  n2love = "";
+  percentage = "";
+
   calculate() {
     let name1 = new Name(this.n1);
     let name2 = new Name(this.n2);
 
-    let names = [name1, name2];
-    let concatenatedNames = (name1.str + name2.str).replace(/ /g, '');
+    // let names = [name1, name2];
+    // let concatenatedNames = (name1.strLowered + name2.strLowered).replace(/ /g, '');
 
-    for (let name of names) {
-      let checkedCharacters = [];
-      for (let s of name.str) {
-        if (checkedCharacters.indexOf(s) < 0 && s != ' ') {
-          var r = new RegExp(s, "g");
-          if ((concatenatedNames.match(r) || []).length > 1) {
-            name.found += (name.str.match(r) || []).length;
-          } else {
-            name.notFound++;
-          }
-          checkedCharacters.push(s);
-        }
-      }
-    }
+    // let i = 0;
+    // for (let name of names) {
+    //   let checkedCharacters = [];
+    //   let concatenatedNames = "";
+    //   if (i == 0) {
+    //     concatenatedNames = name2.strLowered;
+    //   } else {
+    //     concatenatedNames = name1.strLowered;
+    //   }
+    //   for (let s of name.strLowered) {
+    //     if (checkedCharacters.indexOf(s) < 0 && s != ' ') {
+    //       var r = new RegExp(s, "g");
+    //       if ((concatenatedNames.match(r) || []).length > 1) {
+    //         name.charFound += (name.strLowered.match(r) || []).length;
+    //       } else {
+    //         name.charNotFound++;
+    //       }
+    //       checkedCharacters.push(s);
+    //     }
+    //   }
+    //   i++;
+    // }
+
+    name1.find(name2);
+    name2.find(name1);
+
     console.log(name1.toString());
     console.log(name2.toString());
 
     var overallScore = (name1.getTotalScore() + name2.getTotalScore()) / 4;
-    console.log(overallScore);
+    console.log("Total Score: " + overallScore);
+
+    this.n1love = name1.str + " (" + name1.getInitialLoveScore() + " -> " + name1.getFinalLoveScore() + ")";
+    this.n2love = name2.str + " (" + name2.getInitialLoveScore() + " -> " + name2.getFinalLoveScore() + ")";
+
+    this.percentage = "Overall Success in Life: " + overallScore.toString();
   }
 }
 
 export class Name {
-  str: string;
-  found: number = 0;
-  notFound: number = 0;
+  strLowered: string;
+  charFound: number = 0;
+  charNotFound: number = 0;
   scores: number[] = [90, 75, 50, 100];
+  str: string;
 
   constructor(str: string) {
-    this.str = str.toLowerCase();
+    this.str = str.trim();
+    this.strLowered = str.trim().toLowerCase();
   }
 
   getScore(num: number): number {
     let j = 0;
     for (var i = 0; i < num; i++) {
-      j++;
       if (j > 4) {
         j = 1;
       }
+      j++;
+    }
+    if (j > 4) {
+      j = 1;
     }
     return this.scores[j - 1];
   }
 
+  find(name: Name) {
+    for (var c of this.strLowered.replace(/ /g, '')) {
+      if (name.strLowered.search(c) > -1) {
+        this.charFound++;
+      } else {
+        this.charNotFound++;
+      }
+    }
+  }
+
+  getInitialLoveScore(): number {
+    return this.getScore(this.charFound);
+  }
+
+  getFinalLoveScore() : number {
+    return this.getScore(this.charNotFound);
+  }
+
   getTotalScore(): number {
-    var initialScore = this.getScore(this.found);
-    var afterScore = this.getScore(this.notFound);
-    return initialScore + afterScore;
+    var initialScore = this.getInitialLoveScore();
+    var finalScore = this.getFinalLoveScore();
+    return initialScore + finalScore;
   }
 
   toString(): string {
-    var initialScore = this.getScore(this.found);
-    var afterScore = this.getScore(this.notFound);
-    return `[ Name: ${this.str}, Found: ${this.found}, Initial Score: ${initialScore}, Not Found: ${this.notFound}, After Score: ${afterScore}]`;
+    var initialScore = this.getInitialLoveScore();
+    var afterScore = this.getFinalLoveScore();
+    return `[ Name: ${this.str}, Found: ${this.charFound}, Initial Score: ${initialScore}, Not Found: ${this.charNotFound}, After Score: ${afterScore}]`;
   }
 }
